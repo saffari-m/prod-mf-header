c00a79efdef2439a4bc6651a4bcdea50da2f6ed6
mohamadsaffari90@gmail.com <mohamadsaffari90@gmail.com>
2021-11-23 14:51:24 +0330

    Update header repo

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => (
+  <div>
+    Hi there, I'm React from Webpack 5. <h1>Header</h1>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById("app"));
